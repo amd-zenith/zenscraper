5ab53dbed59ceecb0c294c8ba3ce337779f29f7f
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2026-03-08 22:26:17 +0100

    Change file naming convention

--- src/zenscraper/helpers.py
+++ src/zenscraper/helpers.py
@@ -18,9 +18,13 @@ def file_sha256(file: Path):
 
 def ucode_patch_name(patch: Path) -> str:
     # Format should be:
-    # cpuid<cpuid>_rev<revision>_date<yyyymmdd>.bin
+    # cpuid<cpuid>_rev<revision>_date<yyyymmdd>_enc<ee>.bin
     ucodepatch = ucode_patch_parse(patch)
-    return f"family{ucodepatch.header.cpu_family:02x}_cpuid{ucodepatch.header.cpuid_str}_rev{ucodepatch.header.revision:08x}_date{ucodepatch.header.year:04}{ucodepatch.header.month:02}{ucodepatch.header.day:02}.bin"
+    name = f"family{ucodepatch.header.cpu_family:02x}_cpuid{ucodepatch.header.cpuid_str}_rev{ucodepatch.header.update_revision:08x}_date{ucodepatch.header.year:04}{ucodepatch.header.month:02}{ucodepatch.header.day:02}"
+    if ucodepatch.verified_header is not None:
+        name += f"_enc{ucodepatch.verified_header.encrypted:02}"
+    name += ".bin"
+    return name
 
 
 def ucode_patch_process(patch: Path, outdir: Path):
