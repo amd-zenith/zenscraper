d17149b3ac397f51be195ce699661788a891bc97
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2026-06-13 20:32:47 +0200

    Change naming conventions to avoid colisions

--- src/zenscraper/helpers.py
+++ src/zenscraper/helpers.py
@@ -18,11 +18,13 @@ def file_sha256(file: Path):
 
 def ucode_patch_name(patch: Path) -> str:
     # Format should be:
-    # family<family>_cpuid<cpuid>_rev<revision>_date<yyyymmdd>_enc<ee>.bin
+    # family<family>_cpuid<cpuid>_rev<revision>_date<yyyymmdd>_enc<ee>_sha<hash12>.bin
     ucodepatch = ucode_patch_parse(patch)
+    short_hash = file_sha256(patch)[:12]
     name = f"family{ucodepatch.header.cpu_family:02x}_cpuid{ucodepatch.header.cpuid_str}_rev{ucodepatch.header.update_revision:08x}_date{ucodepatch.header.year:04}{ucodepatch.header.month:02}{ucodepatch.header.day:02}"
     if ucodepatch.verified_header is not None:
         name += f"_enc{ucodepatch.verified_header.encrypted:02}"
+    name += f"_sha{short_hash}"
     name += ".bin"
     return name
 
