6c89b7c30fbc2e9f8415f76189ca9c1507a2e56c
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2026-06-13 20:17:10 +0200

    Iterate Platomav commit history

--- src/zenscraper/sources/platomav_cpumicrocodes.py
+++ src/zenscraper/sources/platomav_cpumicrocodes.py
@@ -4,7 +4,8 @@ https://github.com/platomav/CPUMicrocodes
 '''
 
 import git
-from git import Repo
+import shutil
+from git import Repo, Commit
 from pathlib import Path
 from rich.console import Console
 from ..helpers import ucode_patch_process
@@ -27,21 +28,54 @@ def _clone_repo(console: Console, dir: Path) -> Repo:
     return repo
 
 
+def _get_amd_commits(repo: Repo):
+    return list(repo.iter_commits(paths='AMD/'))
+
+
+def _get_amd_patches(workdir: Path, tmpdir: Path, repo: Repo, commit: Commit):
+    # Create a folder for this commit if it does not exist...
+    patch_dir = tmpdir / commit.hexsha
+    patch_dir.mkdir(parents=True, exist_ok=True)
+    # Go to the commit
+    repo.git.checkout(commit.hexsha, force=True)
+    # Copy patches from this snapshot
+    shutil.copytree(str(workdir / "cache" / "CPUMicrocodes" / "AMD"), str(patch_dir), dirs_exist_ok=True)
+
+
 def platomav_cpumicrocodes_scrape(console: Console, workdir: Path, outdir: Path):
     cachedir = workdir / "cache"
-    repo_dir = cachedir / "CPUMicrocodes"
+    tmpdir = workdir / "tmp"
 
     console.log("Scraping the platomav CPUMicrocodes repository...")
     repo = _clone_repo(console, cachedir)
 
-    console.log("Checkout main and ensure we are up to date...")
-    repo.git.checkout("master")
-    repo.git.pull()
+    console.log("Force-sync CPUMicrocodes repo to origin/master (discarding local changes)...")
+    repo.git.checkout("master", force=True)
+    repo.git.reset("--hard")
+    repo.git.clean("-fd")
+    repo.git.fetch("origin", "master")
+    repo.git.reset("--hard", "origin/master")
+
+    console.log("Obtaining the git history for AMD patches...")
+    commits = _get_amd_commits(repo)
+    console.log(f"Found {len(commits)} relevant commits!")
+
+    console.log("Collecting all AMD patches from commit history...")
+    patchesdir = tmpdir / "platomav-amd-patches"
+    if patchesdir.exists():
+        shutil.rmtree(patchesdir)
+    patchesdir.mkdir(parents=True, exist_ok=True)
+    for commit in commits:
+        console.log(f"Fetching commit {commit.hexsha}...")
+        _get_amd_patches(workdir, patchesdir, repo, commit)
 
     console.log("Obtaining all uCode patches...")
-    patches = list(repo_dir.glob("AMD/*.bin"))
+    patches = list(patchesdir.glob("**/*.bin"))
     for patch in patches:
         ucode_patch_process(patch, outdir)
     console.log(f"Processed {len(patches)} uCode patches!")
+
+    if tmpdir.exists():
+        shutil.rmtree(tmpdir)
 
     console.log("The platomav CPUMicrocodes source has been processed!")
