33c1a515f3765898df3617d9505d4b9644be5a6f
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2026-07-12 21:13:23 +0200

    Fix naming bug

--- src/zenscraper/utils/patch.py
+++ src/zenscraper/utils/patch.py
@@ -4,6 +4,7 @@
 from pathlib import Path
 from zenscraper.utils.sha256 import file_sha256
 from amd_ucode_patch.parse import ucode_patch_parse
+from amd_ucode_patch.naming import ucode_patch_name as patch_name
 
 
 def ucode_patch_name(patch: Path) -> str:
@@ -11,9 +12,7 @@ def ucode_patch_name(patch: Path) -> str:
     # family<family>_cpuid<cpuid>_rev<revision>_date<yyyymmdd>_enc<ee>_sha<hash12>.bin
     ucodepatch = ucode_patch_parse(patch)
     short_hash = file_sha256(patch)[:12]
-    name = f"family{ucodepatch.header.cpu_family:02x}_cpuid{ucodepatch.header.cpuid_str}_rev{ucodepatch.header.update_revision:08x}_date{ucodepatch.header.year:04}{ucodepatch.header.month:02}{ucodepatch.header.day:02}"
-    if ucodepatch.verified_header is not None:
-        name += f"_enc{ucodepatch.verified_header.encrypted:02}"
+    name = patch_name(ucodepatch).split(".bin")[0]
     name += f"_sha{short_hash}"
     name += ".bin"
     return name
